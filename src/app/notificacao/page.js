c60d0d65e8db33fb0a913259da5461331e146b86
Pablo Pires <pablo.pires@aluno.ifsp.edu.br>
2025-10-08 14:46:40 -0300

    atualizaçao footer

--- src/app/notificacao/page.js
+++ src/app/notificacao/page.js
@@ -2,25 +2,27 @@ import Notificacao from "../../Components/Notificacao";
 import MenuMob from "../../Components/MenuMobile";
 import HeaderDesk from "../../Components/HeaderDesk";
 import Footer from "@/Components/Footer";
-import styles from './NotificacaoPage.module.css'
+import styles from './NotificacaoPage.module.css';
 
 
 export default function Page() {
   return (
-    <>
+    <div className={styles.pageContainer}> {/* Ensure the page container takes full height */}
       <div className={styles.mobile}>
         <MenuMob />
 
         <Notificacao/>
       </div>
 
-      <div className={styles.desktop}>
+      <div className={styles.desktop}> {/* Wrap desktop content in a flex container */}
         <HeaderDesk />
 
-        <Notificacao />
+        <div className={styles.content}> {/* Ensure content pushes footer to the bottom */}
+          <Notificacao />
+        </div>
 
         <Footer/>
       </div>
-    </>
+    </div>
   );
 }
